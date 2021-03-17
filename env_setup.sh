8c3444c5966b623106c5ef24cadb0e448d1cece0
T4di5 <37825412+T4di5@users.noreply.github.com>
2021-03-17 17:30:17 +0800

    Update env_setup.sh

--- env_setup.sh
+++ env_setup.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install git
 sudo apt-get -y install libc6-dbg libc6-dbg:i386
 sudo apt-get -y install nmap
 sudo apt-get -y install python-pip libssl-dev
-sudo apt-get -y install gdb
+sudo apt-get -y install gdb libffi-dev
 sudo pip install --upgrade capstone -i https://pypi.douban.com/simple
 sudo pip install --upgrade pwntools -i https://pypi.douban.com/simple
 sudo pip install ropgadget -i https://pypi.douban.com/simple
