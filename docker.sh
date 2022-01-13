2e8f16216e70d300bcfd7cabb27c221dd3e27893
T4di5 <37825412+T4di5@users.noreply.github.com>
2022-01-13 17:24:15 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-docker build -t hitcon_training - <<DOCKERFILE_EOF || exit 1
+docker buildx build --platform linux/amd64 -t hitcon_training - <<DOCKERFILE_EOF || exit 1
 from ubuntu:16.04
 run apt-get update
-run apt-get install -y git sudo bash make nano
+run apt-get install -y git sudo bash make nano vim
 run dpkg --add-architecture i386
 run apt-get update
-run git clone https://github.com/T4di5/HITCON-Training /tmp/hitcon
+run git clone https://github.com/scwuaptx/HITCON-Training /tmp/hitcon
 workdir /tmp/hitcon
 run bash env_setup.sh
 run git clone https://github.com/radare/radare2 /tmp/radare2
